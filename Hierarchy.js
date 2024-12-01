b602b13f6ced5b01328c3d5e84be8096ca97965e
Brian Monroy <brian.p.monroy@gmail.com>
2024-12-01 13:30:31 -0800

    hand hierarchy is working!

--- Hierarchy.js
+++ Hierarchy.js
@@ -15,28 +15,27 @@ let hierarchy = ["Straight Flush", "Full House", "Flush", "Straight", "Three of 
 // Two Pair vs Two Pair: the better pair wins; compare the second pair only if first pair is tied
 // Full House vs Full House: highest 3 of a Kind wins; compare the pair only if 3 of a Kind is tied
 
-//console.log(OfAKind(testHand));
-
-let testSort = [5,4,3,2,1]; // [1,2,3,4,5] [3,2,1,5,4] [4,1,5,3,2] [1,1,3,2,2] [5,1,1,1,1] [1,2,1,2,1]
-
-console.log(sortHand([5,4,3,2,1]));
-
-//console.log(straightCheck([1,2,3,4,6]))
-
 // test hands
-let testHighCard = [1,2,3,4,5]; // [5,4,3,2,1] [2,4,1,5,3]
-let testOnePair = [1,2,3,4,1]; // [1,1,2,3,4] [2,3,4,1,1] [2,3,1,4,1] 
-let testTwoPair = [1,1,3,2,2]; // [1,1,2,3,2] [1,1,2,2,3] [1,2,1,2,3] [1,2,1,3,2] [1,2,3,2,1] [3,1,1,2,2] [3,1,2,1,2] [1,2,3,2,1]
-let testThreeOfAKind = [3,2,3,4,3]; // [3,3,2,4,3] [3,2,3,3,4] [3,2,4,3,3] [3,3,2,3,4] [2,4,3,3,3] [3,2,3,4,3] [2,3,3,3,4] [2,3,4,3,3]
-let testFullHouse = [3,3,3,2,2]; // [3,3,2,2,3] [3,2,3,3,2] [3,2,2,3,3] [3,3,2,3,2] [2,2,3,3,3] [3,2,3,2,3] [2,3,3,3,2] [2,3,2,3,3]
+ let testHand = [1,2,3,4,8]; // high card
+// let testHand = [1,2,3,4,1]; // one pair
+// let testHand = [1,1,3,2,2]; // [1,1,2,3,3] [1,1,2,2,3] [1,2,2,3,3] // two pair
+// let testHand = [3,2,3,4,3]; // three of a kind
+// let testHand = [2,3,2,3,3]; // fullhouse
 
-function countRepeats(playerHand, card) {
-    let count = 1;
-    for (j = 1; j < playerHand.length; j++) { 
-        // if (card.value() = playerHand[j].value()) {
-        if (card == playerHand[j]) {
+console.log(OfAKind(testHand));
+
+function countRepeats(playerHand, card, startIndex) {
+    let count = 0;
+
+    for (j = startIndex; j < playerHand.length; j++) { 
+
+        if (playerHand[startIndex] == playerHand[j]) {
             count++;
         }
+        else {
+            break;
+        }
+
     }
     return count;
 }
@@ -46,24 +45,29 @@ function OfAKind(playerHand) {
     let onePair = false;
     let twoPair = false;
     let threeOfAKind = false;
+    let countingArray = playerHand.slice(); // create a copy of array so the manipulations are nondestructive
+    countingArray.sort(); // sort so that the following counting algorithm works
 
     handType = "";
-    for (i = 0; i < playerHand.length - 1; i++) { // loop through the entire hand
-        let count = countRepeats(playerHand, playerHand[i]);
-        console.log("card: " + playerHand[i]);
-        console.log(count);
-        console.log("onePair: " + onePair);
-        if (count == 2) {
-            if (!onePair) { // if a pair has not yet been counted
-                onePair == true;
+
+    for (i = 0; i < countingArray.length - 1; i++) { // loop through the entire hand
+
+        let count = countRepeats(countingArray, countingArray[i], i);
+
+        i += count - 1;
+
+        if (count == 3) {
+            threeOfAKind = true;
+        }
+        else if (count == 2) {
+            if (onePair) { // if a pair has already been counted, this is the second pair
+                twoPair = true;
             }
-            else { // a pair has already been counted, making this the second pair
-                twoPair == true;
+            else { // if a pair has not yet been counted
+                onePair = true;
             }
         }
-        else if (count == 3) {
-            threeOfAKind == true;
-        }
+
     }
     if (threeOfAKind && onePair) { // this is a Full House type of hand
         handType = hierarchy[1];
@@ -83,30 +87,9 @@ function OfAKind(playerHand) {
     return handType;
 }
 
-function sortHand(playerHand) {
-    // sorts the given array in ascending order
-
-    for (i = 0; i < playerHand.length - 1; i++) {
-        let currentMin = playerHand[i];
-        let mindex = i;
-        for (j = i + 1; i < playerHand.length; j++) {
-            if (playerHand[j] < currentMin) {
-                mindex = j;
-                currentMin = playerHand[j];
-            }
-        }
-        if (mindex != i) {
-            playerHand[mindex] = playerHand[i];
-            playerHand[i] = currentMin;
-        }
-    }
-
-    return playerHand;
-}
-
 function straightCheck(playerHand) {
     // checks if hand contains a straight
-    let sorted = sortHand(playerHand.slice(0, (playerHand.length - 1)));
+    let sorted = playerHand.sort() //sortHand(playerHand.slice(0, (playerHand.length - 1)));
     let isStraight = true;
     for (i = 0; i < sorted.length - 1; i++) {
         if ((sorted[i] + 1) != sorted[i + 1]) {
@@ -120,9 +103,26 @@ function flushCheck(playerHand) {
     // checks if hand contains a flush, i.e. all cards have the same symbol
     let isFlush = true;
     for (i = 0; i < playerHand.length - 1; i++) {
-        if (playerHand[i].type != playerHand[i + 1].type) {
+        if (playerHand[i] != playerHand[i + 1]) {
+        //if (playerHand[i].type != playerHand[i + 1].type) {
             return isFlush = false;
         }
     }
     return isFlush;
+}
+
+function bestHand(playerHand) {
+    if (flushCheck(playerHand) && straightCheck(playerHand)) {
+        handType = hierarchy[0];
+    }
+    else if (flushCheck(playerHand)) {
+        handType = hierarchy[2];
+    }
+    else if (straightCheck(playerHand)) {
+        handType = hierarchy[3];
+    }
+    else {
+        handType = OfAKind(playerHand);
+    }
+    return handType;
 }
