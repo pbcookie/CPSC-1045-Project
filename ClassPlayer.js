6f350a0ee6d893cc55f27e19503afe51e49e02ea
Melody-max-ux <melchizedek173@gmail.com>
2024-11-30 21:48:35 -0800

    Update ClassPlayer.js

--- ClassPlayer.js
+++ ClassPlayer.js
@@ -12,6 +12,20 @@ class Player{
         this.handCards = [];
     }
     getHandCards(){return this.handCards;}  //return a reference of the handcard
+    showHandCards(){
+        let arr = this.getHandCards();
+        console.log(
+            (
+                function(){
+                    let result = [];
+                    for(let i=0;i<arr.length;i++){
+                        result.push(arr[i].number + arr[i].type);
+                    }
+                    return result;
+                }
+            )()
+        )
+    }
     getName(){return this.name;} 
     getPoint(){return this.point;}
     addPoint(){this.point += 1;}
