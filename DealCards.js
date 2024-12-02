87cff0664cc964d5be3595ca3750957baa1859c4
Melody-max-ux <melchizedek173@gmail.com>
2024-12-02 15:37:39 -0800

    Update DealCards.js

--- DealCards.js
+++ DealCards.js
@@ -1,8 +1,34 @@
-//Create an array of cards containing three suits for a total of 24 cards. 
-//Create a variable to represent the number of cards in the deck. 
+
+
 //Create a function that  draws the first card from the deck array, returns this card, shifts the remaining cards in the array forward by one position。
 //decreases the deck count by one.
-function dealCards(deck, player){
-  player.addCard(deck.reverse().pop());
+//change the card's location from deck to the player
+
+
+//deck is an array of object cards
+//player is an object of class player
+//num is the nunber of cards that are dealed to the player
+function dealplayerCards(deck, player, num){
+  deck.reverse();
+  for(let i=0;i<num){
+    let newCard =deck.pop(); 
+    player.addCard(newCard);
+    newCard.setLocation(player.handcardLocation[i]); //change the card's location into player's handcard loction
+  }
+  deck.reverse();
+}
+
+
+//deck is an array of object cards
+//table is an array of object cards
+//num is the nunber of cards that are dealed to the table
+//tableLocation is the tableCards loction in ClassCard.js
+function dealTableCards(deck, table, num){
+  deck.reverse();
+  for(let i=0;i<num;i++){
+    let newCard =deck.pop(); 
+    table.push(newCard);
+    newCard.setLocation(tableLocation[i])
+  }
   deck.reverse();
 }
