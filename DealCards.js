8d7203abcad5ba450f2c9475b89ef57bb239de0c
Melody-max-ux <melchizedek173@gmail.com>
2024-12-02 14:53:38 -0800

    Update DealCards.js

--- DealCards.js
+++ DealCards.js
@@ -1,4 +1,8 @@
 //Create an array of cards containing three suits for a total of 24 cards. 
 //Create a variable to represent the number of cards in the deck. 
-//Create a function that randomly draws a card from the deck array, returns this card, shifts the remaining cards in the array forward by one position。
+//Create a function that  draws the first card from the deck array, returns this card, shifts the remaining cards in the array forward by one position。
 //decreases the deck count by one.
+function dealCards(deck, player){
+  player.addCard(deck.reverse().pop());
+  deck.reverse();
+}
