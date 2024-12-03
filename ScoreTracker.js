a2fcb0ac11e5ac73f214a059f8ee90c34054786d
Brian Monroy <brian.p.monroy@gmail.com>
2024-12-03 13:48:39 -0800

    more end of round functions

--- ScoreTracker.js
+++ ScoreTracker.js
@@ -11,36 +11,50 @@ function scoreTracker(player, score) {
             player.removePoint();
         }
     }
-    
+    else if (score == 0) {
+        alert("Tie round!"); // human and computer hands strengths are equal
+    }
 }
 
 function drawScore(player, score) {
     // draws the dots on the score tracker
     let tracker = document.getElementById(player + "tracker");
-    let canvas = document.getElementById("");
+    let canvas = document.getElementById(""); // TODO: replace with the proper canvas element id 
     let ctx = canvas.getContext("2d");
     let score = player.getScore();
     ctx.beginPath;
-    ctx.translate(x + score * 100, y); // replace x and y with proper coordinates
-    ctx.arc(); // draw a small circle in score tracker to represent point change
+    ctx.translate(x + score * 100, y); // TODO: replace x and y with proper coordinates
+    ctx.arc(); // TODO: draw a small circle in score tracker to represent point change
 }
 
 function win() {
+    // called when round ends and the human player wins
     scoreTracker(player, 1);
     alert("You won the hand!");
-    if (ClassPlayer[human].getScore >= 3) {
-        endRound();
-    }
+    endRound()
 }
 
 function lose() {
+    // called when round ends and human player loses
     scoreTracker(player, -1);
     alert("You lost the hand.");
-    if (ClassPlayer[computer].getScore >= 3) {
-        endRound();
-    }
+    endRound();
 }
 
 function endRound() {
-    // 
+    // called after winner/loser/tie is declared in a round or if the player folds
+    if (ClassPlayer[human].getScore >= 3) {
+        endMatch(); 
+    }
+    else if (ClassPlayer[computer].getScore >= 3) {
+        endMatch();
+    }
+    else {
+        // clean up table and set up next round
+        newRound();
+    }
+}
+
+function endMatch() {
+    // TODO: cleanup and prepare for next game
 }
