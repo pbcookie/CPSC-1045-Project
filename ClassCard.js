f34ca423f1796827bc0808c060f863b3d4dba3a4
Melody-max-ux <melchizedek173@gmail.com>
2024-12-04 17:55:45 -0800

    Update ClassCard.js
    adding draw() using drawCard() form card.js

--- ClassCard.js
+++ ClassCard.js
@@ -15,42 +15,41 @@ const thirdCommunityCardLocation = [coodinator_x, coodinator_y];
 const cardLocation = [deckLocation, firstPlayerCardLocation, secondPlayerCardLocation, firstComputerCardLocation, secondComputerCardLocation, firstCommunityCardLocation, secondCommunityCardLocation, thirdCommunityCardLocation];
 const tableLocation = cardLocation.slice(6);// including [firstCommunityCardLocation, secondCommunityCardLocation, thirdCommunityCardLocation]
 class Card{
-  //number is Int  from 1 to 8
-  //type is string, eg:circle
-  //ch is the height of card
-  //cw is the width of card
-  constructor(number, type, ch = 120, cw = 80){
-    this.number = number;
-    this.type = type;
-    this.location = cardLocation[0]; //this card is initial in the deck
-    this.faceUp = false;// this card is face down
-    this.ch = ch;
-    this.cw = cw;
-  }
- 
-  getType(){return this.type;}
-  getNum(){return this.number;}
-  getLocation(){return this.location;}
-  setLocation( newLocation ){this.location = newLocation;}
-  isFace(){return this.faceUp;}  
-  setFace(cover){this.faceUp = cover;}//If you want to reveal this card. use this method to set faceUp to true
-  draw(ctx){
-    if(this.location != cardLocation[0]){//this card is  in deck, no need to be drawed
-      if(this.faceUp){//this card is face up
-        drawCard(this.location[0], this.location[1], this.cw, this.ch, this.number, this.type);
-        // this.location[0] is the coodination-x
-        // this.location[1] is the coodination-y
-      }else{
-        //this card is face down
-        drawback(ctx, this.location[0], this.location[1], this.ch, this.cw);
+    //number is Int  from 1 to 8
+    //type is string, eg:circle
+    //ch is the height of card
+    //cw is the width of card
+    constructor(number, type, ch = 120, cw = 80){
+      this.number = number;
+      this.type = type;
+      this.location = cardLocation[0]; //this card is initial in the deck
+      this.faceUp = false;// this card is face down
+      this.ch = ch;
+      this.cw = cw;
+    }
+   
+    getType(){return this.type;}
+    getNum(){return this.number;}
+    getLocation(){return this.location;}
+    setLocation( newLocation ){this.location = newLocation;}
+    isFace(){return this.faceUp;}  
+    setFace(cover){this.faceUp = cover;}//If you want to reveal this card. use this method to set faceUp to true
+    draw(){
+      if(this.location != cardLocation[0]){//this card is  in deck, no need to be drawed
+        if(this.faceUp){//this card is face up
+          drawCard(this.location[0], this.location[1], this.cw, this.ch, this.number, this.type);
+          // this.location[0] is the coodination-x
+          // this.location[1] is the coodination-y
+        }else{
+          //this card is face down
+          drawback(ctx, this.location[0], this.location[1], this.ch, this.cw);
+        }
       }
     }
+    display(){
+      return this.number + " " + this.type;
+    }
   }
-  display(){
-    return this.number + " " + this.type;
-  }
-}
-
 //draw card 
 //card is the object of class card
 //x , y is the coordinates
